3c72e24c4f836544844dce755c47d830fb6bf33e
Supratik Sengupta <supratik.tukai@gmail.com>
2023-01-15 01:42:01 +0530

    userController updated

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -12,5 +12,8 @@ router.get(
   requireUser,
   UserController.getPostsOfFollowing
 );
+router.get("/getMyPosts", requireUser, UserController.getMyPosts);
+router.get("/getUserPosts", requireUser, UserController.getUserPosts);
+router.delete("/", requireUser, UserController.deleteMyProfile);
 
 module.exports = router;
