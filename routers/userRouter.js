4b300f7d0ee6de6083aa30f689a372d9a31edd59
Supratik Sengupta <supratik.tukai@gmail.com>
2023-01-22 02:25:52 +0530

    cloudinary and new controllers added

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -15,5 +15,8 @@ router.get(
 router.get("/getMyPosts", requireUser, UserController.getMyPosts);
 router.get("/getUserPosts", requireUser, UserController.getUserPosts);
 router.delete("/", requireUser, UserController.deleteMyProfile);
+router.get("/getMyInfo", requireUser, UserController.getMyInfo);
+router.put("/", requireUser, UserController.updateUserProfile);
+router.post("/getUserProfile", requireUser, UserController.getUserProfile);
 
 module.exports = router;
